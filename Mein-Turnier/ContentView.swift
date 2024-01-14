e1991a39b36716a33df224413967a038a0f01682
Patrick Lanham <p.lanham91@icloud.com>
2024-01-14 11:37:25 +0100

    Initial Commit

---
+++ Mein-Turnier/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mein-Turnier
+//
+//  Created by Patrick Lanham on 14.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mein-Turnier/Mein_TurnierApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mein_TurnierApp.swift
+//  Mein-Turnier
+//
+//  Created by Patrick Lanham on 14.01.24.
+//
+
+import SwiftUI
+
+@main
+struct Mein_TurnierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
